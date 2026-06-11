c4b3c09383c33399547ab20f4510719e231d3e56
anubhav-kayal <kayalanubhav14@gmail.com>
2026-06-11 09:53:18 +0530

    feat: export all MongoDB models from single entry point

---
+++ server/src/services/mongo/index.ts
@@ -0,0 +1,9 @@
+export { Repo } from "./models/Repo";
+export { Chunk } from "./models/Chunk";
+export { Conversation } from "./models/Conversation";
+export { IndexJob } from "./models/IndexJob";
+
+export type { IRepo } from "./models/Repo";
+export type { IChunk } from "./models/Chunk";
+export type { IConversation } from "./models/Conversation";
+export type { IIndexJob } from "./models/IndexJob";
